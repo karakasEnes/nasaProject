3a0fd33f5e01026956cd1dd2ad5906403cc35418
Enes Karakas <enes.coding@gmail.com>
2022-11-21 16:19:36 +0300

    cors libary added, whitelist website added to CORS settings

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const cors = require('cors');
 
 const planetRouter = require('./routes/planets/planets.router.js');
 
 const app = express();
+
+const whiteList = ['http://localhost:3000'];
+app.use(
+  cors({
+    origin: function (origin, cb) {
+      if (whiteList.indexOf(origin) !== -1) {
+        cb(null, true);
+      } else {
+        cb(new Error('Not Allowed by CORS!'));
+      }
+    },
+  })
+);
 app.use(express.json());
 app.use(planetRouter);
 
