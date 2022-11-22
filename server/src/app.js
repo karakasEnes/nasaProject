977b460504d021c2fbbf6326be764c23209fed22
Enes Karakas <enes.coding@gmail.com>
2022-11-22 12:11:49 +0300

    scripts arranged for production etc, awesome things

--- server/src/app.js
+++ server/src/app.js
@@ -1,23 +1,38 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const planetRouter = require('./routes/planets/planets.router.js');
 
 const app = express();
 
-const whiteList = ['http://localhost:3000'];
+const whiteList = [
+  'http://localhost:3000',
+  'http://localhost:8181',
+  'http://127.0.0.1:8181',
+];
+// app.use(
+//   cors({
+//     origin: function (origin, cb) {
+//       if (whiteList.indexOf(origin) !== -1) {
+//         cb(null, true);
+//       } else {
+//         cb(new Error('Not Allowed by CORS!'));
+//       }
+//     },
+//   })
+// );
 app.use(
   cors({
-    origin: function (origin, cb) {
-      if (whiteList.indexOf(origin) !== -1) {
-        cb(null, true);
-      } else {
-        cb(new Error('Not Allowed by CORS!'));
-      }
-    },
+    origin: '*',
   })
 );
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(planetRouter);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 module.exports = app;
