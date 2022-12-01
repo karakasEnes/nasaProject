ee118ef73eb9292d24251578be351d61dd9f0b45
Enes Karakas <enes.coding@gmail.com>
2022-12-01 23:11:03 +0300

    launches with front-end

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -14,7 +14,7 @@ function httpAddNewLaunch(req, res) {
     !launch.mission ||
     !launch.rocket ||
     !launch.launchDate ||
-    !launch.destination
+    !launch.target
   ) {
     return res.status(400).json({
       error: 'Missing required launch property ',
