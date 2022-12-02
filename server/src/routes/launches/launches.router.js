461a32ab30fbafdf8118340a7d979f5fad242855
Enes Karakas <enes.coding@gmail.com>
2022-12-02 12:02:09 +0300

    delete / abort launches

--- server/src/routes/launches/launches.router.js
+++ server/src/routes/launches/launches.router.js
@@ -2,11 +2,13 @@ const express = require('express');
 const {
   httpGetAllLaunches,
   httpAddNewLaunch,
+  httpAbortLaunch,
 } = require('./launches.controller');
 
 const launcherRouter = express.Router();
 
 launcherRouter.get('/', httpGetAllLaunches);
 launcherRouter.post('/', httpAddNewLaunch);
+launcherRouter.delete('/:id', httpAbortLaunch);
 
 module.exports = launcherRouter;
